39bb1dc1f01fc33f2e6c5272534c9068aacdbe47
Hiroshi Takeda <takejaponpon+git@gmail.com>
2024-08-23 01:05:50 +0900

    chore: Update eslint and prettier configurations

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,12 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import typescriptParser from '@typescript-eslint/parser'
-import typescriptPlugin from '@typescript-eslint/eslint-plugin'
-import prettier from 'eslint-plugin-prettier'
-import prettierConfig from 'eslint-config-prettier'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import typescriptParser from '@typescript-eslint/parser';
+import typescriptPlugin from '@typescript-eslint/eslint-plugin';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default [
   { ignores: ['dist'] },
@@ -31,19 +31,13 @@ export default [
       '@typescript-eslint': typescriptPlugin,
       prettier,
     },
-    extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:prettier/recommended',
-      'prettier',
-    ],
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       ...typescriptPlugin.configs.recommended.rules,
+      ...prettierConfig.rules,
       'prettier/prettier': 'error',
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
@@ -52,4 +46,4 @@ export default [
       ],
     },
   },
-]
+];
--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,4 +1,6 @@
+import React from 'react';
+
 declare module '*.svg' {
-  const content: any;
+  const content: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
   export default content;
 }
