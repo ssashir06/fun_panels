d003cf1c4d76fd7f904b0b2ec10c7b31ee4fbcbc
Hiroshi Takeda <takejaponpon+git@gmail.com>
2024-08-25 16:13:19 +0900

    chore: Update eslint and prettier configurations for ordering of import

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,6 @@
 import js from '@eslint/js';
 import typescriptPlugin from '@typescript-eslint/eslint-plugin';
 import typescriptParser from '@typescript-eslint/parser';
-import prettierConfig from 'eslint-config-prettier';
-import prettier from 'eslint-plugin-prettier';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
@@ -29,7 +27,6 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      prettier,
       'simple-import-sort': simpleImportSort,
     },
     rules: {
@@ -37,14 +34,13 @@ export default [
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      ...prettierConfig.rules,
-      'prettier/prettier': 'error',
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
       'simple-import-sort/imports': 'error',
+      'quotes': ['error', 'single'],
     },
   },
   {
@@ -66,7 +62,6 @@ export default [
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
       '@typescript-eslint': typescriptPlugin,
-      prettier,
       'simple-import-sort': simpleImportSort,
     },
     rules: {
@@ -75,14 +70,13 @@ export default [
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       ...typescriptPlugin.configs.recommended.rules,
-      ...prettierConfig.rules,
-      'prettier/prettier': 'error',
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
       'simple-import-sort/imports': 'error',
+      'quotes': ['error', 'single'],
     },
   },
 ];
