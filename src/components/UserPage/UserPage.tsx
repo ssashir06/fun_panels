9117a2d2e52cbff22faa4e90c214af86595f7f39
Hiroshi Takeda <takejaponpon+git@gmail.com>
2024-08-25 20:57:07 +0900

    feat: Add react-router-dom dependency and implement routing in main.tsx

---
+++ src/components/UserPage/UserPage.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect,useState } from 'react';
+
+const UserPage: React.FC = () => {
+  const [userName, setUserName] = useState('');
+
+  useEffect(() => {
+    const storedUserName = sessionStorage.getItem('userName');
+    if (storedUserName) {
+      setUserName(storedUserName);
+    }
+  }, []);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newUserName = e.target.value;
+    setUserName(newUserName);
+    sessionStorage.setItem('userName', newUserName);
+  };
+
+  return (
+    <div>
+      <h1>User Page</h1>
+      <input
+        type="text"
+        value={userName}
+        onChange={handleChange}
+        placeholder="Enter your name"
+      />
+    </div>
+  );
+};
+
+export default UserPage;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,26 @@
 import './index.css';
 
-import { StrictMode } from 'react';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 
 import App from './App';
+import UserPage from './components/UserPage/UserPage';
 
-createRoot(document.getElementById('root') as HTMLElement).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+root.render(
+  <React.StrictMode>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/user">User Page</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/user" element={<UserPage />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
 );
