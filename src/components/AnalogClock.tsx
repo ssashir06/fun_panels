327527580d58fe6123e7b21e5c9b6a6cb1e0e00f
Hiroshi Takeda <takejaponpon+git@gmail.com>
2024-08-27 21:58:08 +0900

    feat: Add hour labels to AnalogClock component

--- src/components/AnalogClock.tsx
+++ src/components/AnalogClock.tsx
@@ -22,6 +22,10 @@ const AnalogClock: React.FC = () => {
       <div style={{ ...styles.hand, ...styles.hourHand, transform: `rotate(${hoursDegrees}deg)` }} />
       <div style={{ ...styles.hand, ...styles.minuteHand, transform: `rotate(${minutesDegrees}deg)` }} />
       <div style={{ ...styles.hand, ...styles.secondHand, transform: `rotate(${secondsDegrees}deg)` }} />
+      <div style={{ ...styles.hourLabel, ...styles.label3 }}>3</div>
+      <div style={{ ...styles.hourLabel, ...styles.label6 }}>6</div>
+      <div style={{ ...styles.hourLabel, ...styles.label9 }}>9</div>
+      <div style={{ ...styles.hourLabel, ...styles.label12 }}>12</div>
     </div>
   );
 };
@@ -58,6 +62,34 @@ const styles = {
     width: '40%',
     left: '10%',
     backgroundColor: 'red',
+  },
+  hourLabel: {
+    position: 'absolute' as const,
+    fontSize: '16px',
+    fontWeight: 'bold' as const,
+    textAlign: 'center' as const,
+    width: '20px',
+    height: '20px',
+  },
+  label3: {
+    top: '50%',
+    left: '90%',
+    transform: 'translate(-50%, -50%)',
+  },
+  label6: {
+    top: '90%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  },
+  label9: {
+    top: '50%',
+    left: '10%',
+    transform: 'translate(-50%, -50%)',
+  },
+  label12: {
+    top: '10%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
   },
 };
 
